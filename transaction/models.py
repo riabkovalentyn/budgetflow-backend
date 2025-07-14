e5bafb868586c003c972d2c755c7d4c9bcff77e0
Valentyn Riabko <riabkovalentyn@gmail.com>
2025-07-14 18:47:53 +0200

    Fix model name typo and update requirements; add serializers for Transaction and Goal

--- transaction/models.py
+++ transaction/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Transaktion(models.Model):
+class Transaction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     type = models.CharField(max_lenght = 10, choices = [
         ('income', 'Income'),
---
+++ transaction/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Transaction, Goal
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+
+class GoalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Goal
+        fields = '__all__'       
