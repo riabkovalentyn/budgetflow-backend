60ae99a52f4c490845bee5e6ed516d4f79fc8cbf
Valentyn Riabko <riabkovalentyn@gmail.com>
2025-07-18 21:53:11 +0200

    Implement JWT authentication and refactor transaction and goal views; update URLs and requirements

--- transaction/urls.py
+++ transaction/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from . import views
-from .views import TransactionListCreateView
+
 
 
 urlpatterns = [
-    path('list/', TransactionListCreateView.as_view(), name='transaction-list-create'),
+    path('goals/', views.GoalViewSet.as_view({'get': 'list', 'post': 'create'}), name='goal-list-create'),
+    path('goals/<int:pk>/', views.GoalViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='goal-detail'),
 ]
 
 
---
+++ transaction/views/goal.py
@@ -0,0 +1,41 @@
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework import status
+from ..models import  Goal
+from ..serializers import GoalSerializer
+
+class GoalViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing goal instances.
+    """
+    queryset = Goal.objects.all()
+    serializer_class = GoalSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
